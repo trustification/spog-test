ff779ff8233cb3f4bb5f04c9137327b0bfc36c7e
matejnesuta <matejnetusa@gmail.com>
2024-09-10 11:10:58 +0200

    add csaf verification

--- tests/fixtures/upload.py
+++ tests/fixtures/upload.py
@@ -23,13 +23,13 @@ def drag_and_drop(page:Page, file_path:str, drop_zone_selector: str) -> None:
     # Dispatch the 'drop' event on the target element
     page.dispatch_event(drop_zone_selector, 'drop', {"dataTransfer": data_transfer})
 
-def file_is_loaded(page:Page,type:str):
+def file_is_loaded(page:Page,type:str,article:str):
     page.locator("h1").get_by_text(f"Get started by uploading your {type} file").is_hidden()
-    load_button = page.locator("button").get_by_text(f"Load an {type}").is_hidden()
+    load_button = page.locator("button").get_by_text(f"Load {article} {type}").is_hidden()
 
-def file_is_not_loaded(page:Page,type:str):
+def file_is_not_loaded(page:Page,type:str,article:str):
     page.locator("h1").get_by_text(f"Get started by uploading your {type} file").is_visible()
-    load_button = page.locator("button").get_by_text(f"Load an {type}")
+    load_button = page.locator("button").get_by_text(f"Load {article} {type}")
     load_button.is_visible()
     load_button.is_enabled()
 
@@ -46,12 +46,12 @@ def verify_upload(page: Page,type:str,file_name):
     page.locator("h1").get_by_text(f"Upload {article} {type}").is_visible()
     # Verify the Upload page loaded with input text area with a Load button
     # Verify Load button is visible and enabled
-    file_is_not_loaded(page,type)
+    file_is_not_loaded(page,type,article)
     # Verify user is able to select a file by hovering
     file_path = get_file_path(file_name,'testdata/'+type.lower()+'/') 
     drag_and_drop(page, file_path, "input[type='file']")
     # Verify the input text area loaded with contents
-    file_is_loaded(page,type)
+    file_is_loaded(page,type,article)
     monacoEditor = page.locator(".monaco-editor").nth(0)
     monacoEditor.is_visible()
     monacoEditor.click()
@@ -59,4 +59,4 @@ def verify_upload(page: Page,type:str,file_name):
     page.get_by_role("alert").get_by_text("Cannot edit in read-only editor").is_visible()
     # Verify Clear button clears the contents on the text area
     page.locator("#file-clear").click()
-    file_is_not_loaded(page,type)
+    file_is_not_loaded(page,type,article)
